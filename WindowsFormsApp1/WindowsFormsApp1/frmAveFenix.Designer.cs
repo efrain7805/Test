d0561c938e10e008150bcd4237b5e3f0d402bc31
Alejandro Sepúlveda Cuartas <compuviajero@hotmail.com>
2019-08-18 15:52:12 -0600

    boton agregado a fenix

--- WindowsFormsApp1/WindowsFormsApp1/frmAveFenix.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/frmAveFenix.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Erase una vez un ave fenix que aún no había almorzado.";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmAveFenix
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "frmAveFenix";
             this.Text = "frmAveFenix";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
